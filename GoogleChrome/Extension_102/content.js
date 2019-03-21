15212f8157b784a9bc7f0197b899719ab2d6805f
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2019-03-21 23:00:34 +0530

    try 2 to get data
    try 2 to get data

--- GoogleChrome/Extension_102/content.js
+++ GoogleChrome/Extension_102/content.js
@@ -4,39 +4,33 @@ function createButtonAndAddit_Akbar(){
 
     var getflightprintticket =  $(".flightprintticket");
 
-    for (let index = 0; index < getflightprintticket.length; index++) {
-        const element = getflightprintticket[index];
-        
-        var div = document.createElement( 'div' );
-        var btnForm = document.createElement( 'form' );
-        var btn = document.createElement( 'input' );
+    var ref = $(".referenceno > strong > a");
+    var div = document.createElement( 'div' );
+    var btnForm = document.createElement( 'form' );
+    var btn = document.createElement( 'input' );
 
-        //append all elements
-        $(element).after(div);
-        div.appendChild( btnForm );
-        btnForm.appendChild( btn );
+    //append all elements
+    $(ref).after(div);
+    div.appendChild( btnForm );
+    btnForm.appendChild( btn );
 
-        //set attributes for div
-        div.id = 'myButtonDivID_'+index;
-        var _position = $(element).position();
-         
-        div.style.cssText = "left:"+(_position.left)+"top:"+(_position.top+100)+" position:absolute;z-index:1000000000;"
-        //div.style.cssText = "position:absolute;z-index:1000000000;"
-        div.style.backgroundColor = 'black';
+    //set attributes for div
+    div.id = 'myButtonDivID_'+1;
+    var _position = $(ref).position();
+     
+    div.style.cssText = "left:"+(_position.left)+"top:"+(_position.top+100)+" position:absolute;z-index:1000000000;"
+    //div.style.cssText = "position:absolute;z-index:1000000000;"
+    div.style.backgroundColor = 'black';
 
-        //set attributes for btnForm
-        btnForm.action = '';
+    //set attributes for btnForm
+    btnForm.action = '';
 
-        //set attributes for btn  
-        btn.id ="button_"+index;  
-        btn.type = 'button';
-        btn.value = 'Get Data';
-        btn.classList.add("statusconfirmed");
-        btn.classList.add("_GetDataBtn");
-        //btn.addEventListener("click",);
-        
-    } 
-    
+    //set attributes for btn  
+    btn.id ="button_"+1;  
+    btn.type = 'button';
+    btn.value = 'Get Data';
+    btn.classList.add("statusconfirmed");
+    btn.classList.add("_GetDataBtn");    
 }
 
 
@@ -45,33 +39,39 @@ $(document).ready(function(){
    
     createButtonAndAddit_Akbar();
 
-    $("._GetDataBtn").bind("click",function(e){
-        var _data = $("#"+e.target.id).closest(".flightdetails");
-        var _MainParent = $("#"+e.target.id).parents("div > .rightitem");
+    $("._GetDataBtn").bind("click",function(e){       
+        var _MainDiv =  $(".rightitem > .flightdetails");
 
-        var _Referenceno = $(".referenceno > strong > a").text();       
-        var _PNRNo = $(_MainParent).find('.itemheadersub > .mainheadright > .headernav:eq(0)').text().split("|")[1].split(":")[1];
-        var _AirlineID =  $(_MainParent).find('.airline > .item:eq(1)').html().split("<br>")[0];
-        var _AirlineName =  $(_MainParent).find(".airline > .item:eq(1)").html().split("<br>")[1];
-        var _Refund  = $(_MainParent).find(".refund").attr("title");
-        var _FlightType =  $(_MainParent).find(".flighttype").attr("title");
-        var _BookingType = $(_MainParent).find(".airline > .item:eq(2)").text();
-              
-        var jsonData = {
-            "TicketDetails" : [
-               {
-                    "_Referenceno" : _Referenceno,                   
-                    "_PNRNo" :_PNRNo,
-                    "_AirlineID" : _AirlineID,
-                    "_AirlineName" :_AirlineName,
-                    "_Refund" : _Refund,
-                    "_FlightType" : _FlightType,
-                    "_BookingType" :_BookingType  
-                }
-            ]        
-        };
+        console.log(_MainDiv);
 
-        console.log(jsonData)
+        var _MainParent = $(_MainDiv).parents(".rightitem").find(".itemheadersub > .mainheadright > .headernav:eq(0)");
+        console.log($(_MainParent));
+        // var _Referenceno = $(".referenceno > strong > a").text();       
+        // var _PNRNo = $(_MainParent).find('.itemheadersub > .mainheadright > .headernav:eq(0)').text().split(":")[3];
+        // var _AirlineID =  $(_MainParent).find('.airline > .item:eq(1)').html().split("<br>")[0];
+        // var _AirlineName =  $(_MainParent).find(".airline > .item:eq(1)").html().split("<br>")[1];
+        // var _Refund  = $(_MainParent).find(".refund").attr("title");
+        // var _FlightType =  $(_MainParent).find(".flighttype").attr("title");
+        // var _BookingType = $(_MainParent).find(".airline > .item:eq(2)").text();
+
+
+
+        
+        // var jsonData = {
+        //     "TicketDetails" : [
+        //        {
+        //             "_Referenceno" : _Referenceno,                   
+        //             "_PNRNo" :_PNRNo,
+        //             "_AirlineID" : _AirlineID,
+        //             "_AirlineName" :_AirlineName,
+        //             "_Refund" : _Refund,
+        //             "_FlightType" : _FlightType,
+        //             "_BookingType" :_BookingType  
+        //         }
+        //     ]        
+        // };
+
+        // console.log(jsonData)
     });  
 });
 
@@ -83,9 +83,50 @@ $(document).ready(function(){
 
 
 
+// var _data = $("#"+e.target.id).closest(".flightdetails");
+// var _MainParent = $("#"+e.target.id).parents("div > .rightitem");
+
+// var _Referenceno = $(".referenceno > strong > a").text();       
+// var _PNRNo = $(_MainParent).find('.itemheadersub > .mainheadright > .headernav:eq(0)').text().split("|")[1].split(":")[1];
+// var _AirlineID =  $(_MainParent).find('.airline > .item:eq(1)').html().split("<br>")[0];
+// var _AirlineName =  $(_MainParent).find(".airline > .item:eq(1)").html().split("<br>")[1];
+// var _Refund  = $(_MainParent).find(".refund").attr("title");
+// var _FlightType =  $(_MainParent).find(".flighttype").attr("title");
+// var _BookingType = $(_MainParent).find(".airline > .item:eq(2)").text();
 
 
+// for (let index = 0; index < getflightprintticket.length; index++) {
+//     const element = getflightprintticket[index];
+    
+//     var div = document.createElement( 'div' );
+//     var btnForm = document.createElement( 'form' );
+//     var btn = document.createElement( 'input' );
 
+//     //append all elements
+//     $(element).after(div);
+//     div.appendChild( btnForm );
+//     btnForm.appendChild( btn );
+
+//     //set attributes for div
+//     div.id = 'myButtonDivID_'+index;
+//     var _position = $(element).position();
+     
+//     div.style.cssText = "left:"+(_position.left)+"top:"+(_position.top+100)+" position:absolute;z-index:1000000000;"
+//     //div.style.cssText = "position:absolute;z-index:1000000000;"
+//     div.style.backgroundColor = 'black';
+
+//     //set attributes for btnForm
+//     btnForm.action = '';
+
+//     //set attributes for btn  
+//     btn.id ="button_"+index;  
+//     btn.type = 'button';
+//     btn.value = 'Get Data';
+//     btn.classList.add("statusconfirmed");
+//     btn.classList.add("_GetDataBtn");
+//     //btn.addEventListener("click",);
+    
+// } 
 
 
 
